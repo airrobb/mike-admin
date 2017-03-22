d82c82b50b60b77aa44a386b492a899ead6227d1
FilmKnurd <andrew@andrewelster.com>
2017-03-22 13:29:18 -0400

    Bumps version; Uses version dynamically in CLI

--- bin/mike-admin.js
+++ bin/mike-admin.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
+var pjson = require('../package.json');
 var program = require('commander');
 var printGreeting = require('../index.js').mikeAdmin;
 var mikeGyver = require('../lib/mikegyver.js');
 
-program.version('1.0.5').option('-p, --play-theme', 'Play MikeGyver theme song').parse(process.argv);
+program.version(pjson.version).option('-p, --play-theme', 'Play MikeGyver theme song').parse(process.argv);
 
 if (process.argv.length < 3) {
 	printGreeting();
